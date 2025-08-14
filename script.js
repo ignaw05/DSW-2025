33621a6b36f741d0491bac12595fdb9ede3b4340
ignaciowuilloud <ignaciowuilloud@gmail.com>
2025-08-13 21:30:38 -0300

    Add github link

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const secreto = document.getElementById("secreto")
 const imgSec = document.getElementById("imgSec")
 
 secreto.addEventListener("click", () => {
-    imgSec.innerHTML = `<h3>Soy colorado</h3>
+    imgSec.innerHTML = `<h3>Soy colorado🍀🎱</h3>
     <img src="images-3.jpeg">`
 })
